ff588039146372c70242a7762fe10ce868ec69e8
Kevin Plattret <kevin@plattret.com>
2020-03-27 17:19:20 +0000

    [Elixir] Rotational Cipher
    Creates an implementation of the rotational cipher, also sometimes
    called the Caesar cipher.
    
    I'm using codepoints here, which I think makes it a little bit more
    performant than strings. By checking each character against a list of
    alphabet letters, both in lowercase and uppercase, I can make sure that
    only letters are rotated and other characters remain the same.
    
    The one caveat I can think of with this approach is that it wouldn't
    work properly with special characters such as accented letters.
    
    https://exercism.io/tracks/elixir/exercises/rotational-cipher

---
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -0,0 +1,37 @@
+defmodule RotationalCipher do
+  @lowercase 'abcdefghijklmnopqrstuvwxyz'
+  @uppercase 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> to_charlist
+    |> rotate_when_relevant(shift)
+    |> to_string
+  end
+
+  defp rotate_when_relevant(charlist, shift) do
+    Enum.map(charlist, fn char ->
+      cond do
+        Enum.member?(@lowercase, char) -> apply_rotation(@lowercase, char, shift)
+        Enum.member?(@uppercase, char) -> apply_rotation(@uppercase, char, shift)
+        true -> char
+      end
+    end)
+  end
+
+  defp apply_rotation(list, char, shift) do
+    old_index = Enum.find_index(list, &(&1 == char))
+    new_index = old_index + shift
+    new_index = if new_index > 25, do: new_index - 26, else: new_index
+
+    Enum.at(list, new_index)
+  end
+end

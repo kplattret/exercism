c2ad698eeb4d197efd83cd0831eba4e202b4c8c6
Kevin Plattret <kevin@plattret.com>
2023-07-28 20:07:21 +0100

    [TypeScript] Resistor Color Duo
    My simple solution to this challenge. It iterates through the list of
    band colours and gets the index value of the first two given colours.
    
    https://exercism.org/tracks/typescript/exercises/resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,9 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colors: string[]): number {
+  const band_colors = [
+    "black", "brown", "red", "orange", "yellow",
+    "green", "blue", "violet", "grey", "white"
+  ]
+  const color_indexes = [...colors.slice(0, 2)].map(color => band_colors.indexOf(color))
+
+  return Number(color_indexes.join(''))
 }
